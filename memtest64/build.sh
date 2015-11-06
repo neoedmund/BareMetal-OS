9fbcd625eff05d1ff48fec03e33fccaed6d630c7
neoe <neoedmund@gmail.com>
2015-11-07 05:49:45 +0800

    clear

--- memtest64/build.sh
+++ memtest64/build.sh
@@ -2,19 +2,21 @@
 set -e 
 
 cd pure64
-nasm linuxBoot.s -o ../bin/linuxBoot.bin
-
+nasm linuxBoot.s -o ../bin/linuxBoot.o
 
 cd ../bmos
-nasm -f elf64 kernel64.asm -o ../bin/kernel64.elf64
+nasm -f elf64 kernel64.asm -o ../bin/kernel64.o
 gcc  -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o ../bin/libBareMetal.o libBareMetal.c
 gcc  -std=c99 -c -m64 -nostdlib -nostartfiles -nodefaultlibs -fomit-frame-pointer -mno-red-zone -o ../bin/neos.o neos.c 
-cp kernel64.lds ../bin/
-cd  ../bin/
-ld -T kernel64.lds kernel64.elf64 neos.o libBareMetal.o -b binary  -o kernel64.bin
 
-cat linuxBoot.bin kernel64.bin > neos.bin
-sudo cp -v neos.bin /boot
+cd ../pw_mem
+make
+
+cd  ../bin/
+ld -T ../bmos/kernel64.lds kernel64.o neos.o libBareMetal.o -b binary  -o kernel64all.o
+
+cat linuxBoot.o kernel64all.o > neos.img
+sudo cp -v neos.img /boot
 ls -l 
 
 echo "OK"
